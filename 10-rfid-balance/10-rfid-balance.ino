22019882864d24be1a9d83c2678c8873c9b6a30a
Philippe Jadin <philippe.jadin@gmail.com>
2018-06-26 10:47:57 +0200

    code debug balance

--- 10-rfid-balance/10-rfid-balance.ino
+++ 10-rfid-balance/10-rfid-balance.ino
@@ -1,7 +1,7 @@
 /*
 
- Cette version utilise fadeled
- 
+  Cette version utilise fadeled
+
    PINS arduino utilisées :
    - serial : 0, 1
    - leds : 3
@@ -10,20 +10,24 @@
    - i2c : A4 (SDA), A5 (SCL)
 
    Cablage switch :
-  - blanc : GND
-  - brun : VCC
-  - vert : mosfet 1 / pin 5
-  - jaune : mosfet 2 / pin 6
+  - Brun : Switch 5V
+  - Jaune : Switch (pin 6)
+  - Blanc : LED -
+  - Vert : LED + (12V!) (pin 5 en pwm sur l'arduino)
+
+
 
 */
 
 const char MODULE_NAME[] = "10-rfid-balance"; // à changer pour chaque module, pour l'identifier facilement, à mettre en début de sketch
 #include <bearlib.h> // à inclure en dernier
 
-
+const int switchPin = 6; // switch déclencheur de la balance
+int buttonState = 0; 
 
 void setup() {
   bear_init();
+  pinMode(switchPin, INPUT);
 }
 
 
@@ -35,6 +39,18 @@ void loop() {
   bear_led_standby(); // les leds se mettent à clignoter doucement, mode attente,
 
 
+  // code debug
+  buttonState = digitalRead(switchPin);
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    analogWrite(LED_PIN, LED_HIGH);
+  } else {
+    // turn LED off:
+    analogWrite(LED_PIN, LED_LOW);
+  }
+  // fin code debug
+
   // Attend une carte RFID
   if (bear_has_card()) {
 
@@ -42,12 +58,15 @@ void loop() {
     bear_stop();
     bear_led_blink();
 
+    for (int i = 0; i < 10; i++)
+    {
+      analogWrite(5, 255);
+      delay(500);
 
+      analogWrite(5, 0);
+      delay(500);
 
-  } else {
-
-    bear_delay(100);
-    bear_stop();
+    }
 
   }
 
