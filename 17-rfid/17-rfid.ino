a6094ccb9f8e35dc71f61ba5aa8a606a0d6747de
leresteux <leresteuxj@gmail.com>
2018-10-05 14:27:02 +0200

    update 17 et 18

--- 17-rfid/17-rfid.ino
+++ 17-rfid/17-rfid.ino
@@ -11,12 +11,13 @@ const char MODULE_NAME[] = "17-rfid"; // à changer pour chaque module, pour l'i
 #include <bearlib.h> // à inclure en dernier
 
 int count = 0;
+int last_count;
 
 void setup() {
   bear_init();
-  Serial.println("play 17-background.png");
+  Serial.println("play 17-bkg.png");
   Serial.println("loop 17-idle.h264");
-  
+
 }
 
 
@@ -33,50 +34,56 @@ void loop() {
 
     bear_stop();
 
-    count ++;
-
-    if (count == 6)
-    {
-      count = 1;
+    count = random(1, 6);
+    if (count == last_count) {
+      if (count == 5) {
+        count = 1;
+      }
+      else {
+        count ++;
+      }
     }
+    /*count ++;
 
-    analogWrite(LED_PIN, LED_HIGH);
+      if (count == 6)
+      {
+      count = 1;
+      }
+    */
+    bear_led_blink();
 
     if (count == 1)
     {
       Serial.println("play 17-option1.wav");
       Serial.println("play 17-option1.h264");
-      bear_delay(25000);
+      bear_delay(21000);
     }
     if (count == 2)
     {
       Serial.println("play 17-option2.wav");
       Serial.println("play 17-option2.h264");
-      bear_delay(28000);
+      bear_delay(24000);
     }
     if (count == 3)
     {
       Serial.println("play 17-option3.wav");
       Serial.println("play 17-option3.h264");
-      bear_delay(30000);
+      bear_delay(26000);
     }
     if (count == 4)
     {
       Serial.println("play 17-option4.wav");
       Serial.println("play 17-option4.h264");
-      bear_delay(25000);
+      bear_delay(21000);
     }
     if (count == 5)
     {
       Serial.println("play 17-option5.wav");
       Serial.println("play 17-option5.h264");
-      bear_delay(25000);
+      bear_delay(21000);
     }
 
-    
-    analogWrite(LED_PIN, LED_LOW);
-
-
+last_count= count;
     Serial.println("loop 17-idle.h264");
 
 
