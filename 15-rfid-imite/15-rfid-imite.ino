2b9b32ded25c93ff5980d3511413466e752f84bf
Philippe Jadin <philippe.jadin@gmail.com>
2018-10-12 13:06:44 +0200

    timeout resetté

--- 15-rfid-imite/15-rfid-imite.ino
+++ 15-rfid-imite/15-rfid-imite.ino
@@ -19,10 +19,13 @@ const char MODULE_NAME[] = "15-rfid"; // à changer pour chaque module, pour l'i
 #include <bearlib.h> // à inclure en dernier
 
 int count = 1;
+unsigned long timeout;
+
 
 void setup() {
   bear_init();
   Serial.println("loop 15-action1.h264");
+  timeout = millis();
 }
 
 
@@ -36,57 +39,68 @@ void loop() {
 
   // Attend une carte RFID
   if (bear_has_card()) {
-
-
     bear_stop();
-
-    count ++;
-
-    if (count > 9)
-    {
-      count = 1;
-    }
-
-    if (count == 1)
-    {
-      Serial.println("loop 15-action1.h264");
-    }
-    if (count == 2)
-    {
-      Serial.println("loop 15-action2.h264");
-    }
-    if (count == 3)
-    {
-      Serial.println("loop 15-action3.h264");
-    }
-    if (count == 4)
-    {
-      Serial.println("loop 15-action4.h264");
-    }
-    if (count == 5)
-    {
-      Serial.println("loop 15-action5.h264");
-    }
-    if (count == 6)
-    {
-      Serial.println("loop 15-action6.h264");
-    }
-    if (count == 7)
-    {
-      Serial.println("loop 15-action7.h264");
-    }
-    if (count == 8)
-    {
-      Serial.println("loop 15-action8.h264");
-    }
-    if (count == 9)
-    {
-      Serial.println("loop 15-action9.h264");
-    }
-  
-
+    show_image();
     bear_led_blink();
     bear_delay(500);
   }
 
+  if (millis() > timeout + 60000)
+  {
+    show_image();
+    timeout = millis();
+  }
+
 }
+
+
+void show_image() // affiche un des ours chaque fois un différent
+{
+  count ++;
+
+  if (count > 9)
+  {
+    count = 1;
+  }
+
+  if (count == 1)
+  {
+    Serial.println("loop 15-action1.h264");
+  }
+  if (count == 2)
+  {
+    Serial.println("loop 15-action2.h264");
+  }
+  if (count == 3)
+  {
+    Serial.println("loop 15-action3.h264");
+  }
+  if (count == 4)
+  {
+    Serial.println("loop 15-action4.h264");
+  }
+  if (count == 5)
+  {
+    Serial.println("loop 15-action5.h264");
+  }
+  if (count == 6)
+  {
+    Serial.println("loop 15-action6.h264");
+  }
+  if (count == 7)
+  {
+    Serial.println("loop 15-action7.h264");
+  }
+  if (count == 8)
+  {
+    Serial.println("loop 15-action8.h264");
+  }
+  if (count == 9)
+  {
+    Serial.println("loop 15-action9.h264");
+  }
+
+   timeout = millis();
+
+}
+
